9f72f8bfa289ab6fea9a7800aeec86bbeef69cc7
Moritz <78215816+MoritzSkrepek@users.noreply.github.com>
2024-10-07 16:16:52 +0200

    Listboxen, Datenbank
    Listboxen hinzugefuegt und werden auch schon angezeigt. Datenbank etwas hinzugefuegt

--- WatchlistApp/MainWindow.xaml.cs
+++ WatchlistApp/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using DataModel;
+using LinqToDB;
+using LinqToDB.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +20,68 @@ namespace WatchlistApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private DataConnection connection = new DataConnection("SQLite", "Data Source=WatchlistDatabase.db");
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<WatchlistShow> watchlist_shows { get; set; }
+
+        public ObservableCollection<Watchlist> _watchlists;
+        public ObservableCollection<Watchlist> watchlists 
+        {
+            get { return _watchlists; }
+            set 
+            { 
+                _watchlists = value;
+                OnPropertyChanged(nameof(watchlists));
+            }
+        }
+
+        public ObservableCollection<Show> _shows;
+        public ObservableCollection<Show> shows 
+        {
+            get { return _shows; }
+            set
+            {
+                _shows = value;
+                OnPropertyChanged(nameof(shows));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            GetLists();
+            DataContext = this;
+        }
+
+        //Get Watchlist Table contents from Database and convert to ObservableCollection for visualisation with ListBox
+        private void GetLists()
+        {
+            var watchlists_ = connection.GetTable<Watchlist>().ToList();
+            watchlists = new ObservableCollection<Watchlist>(watchlists_); 
+        }
+
+        private void WatchlistSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Check if an item is selected
+            if (watchlist_listbox.SelectedItem is Watchlist selectedWatchlist)
+            {
+                // Fetch the shows associated with the selected watchlist
+                var showsForSelectedWatchlist = connection.GetTable<WatchlistShow>()
+                    .Where(ws => ws.WlNr == selectedWatchlist.WlNr)
+                    .Join(connection.GetTable<Show>(),
+                        ws => ws.ShowNr,
+                        show => show.ShowNr,
+                        (ws, show) => show)
+                    .ToList();
+                shows = new ObservableCollection<Show>(showsForSelectedWatchlist);
+            }
         }
     }
 }
--- WatchlistApp/obj/Debug/net8.0-windows/WatchlistApp.AssemblyInfo.cs
+++ WatchlistApp/obj/Debug/net8.0-windows/WatchlistApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchlistApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22acc32941dfa54396cb7c49fc42d3241f38f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchlistApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchlistApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
